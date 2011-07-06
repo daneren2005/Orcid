ce74b69901bf17cdf372cca6c731cba7e15c77cd
Scott Jackson <daneren2005@yahoo.com>
2011-07-05 19:07:37 -0700

    Changed netbeans project properties

---
+++ nbproject/Makefile-Release_Linux.mk
@@ -0,0 +1,123 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+AS=as
+
+# Macros
+CND_PLATFORM=GNU-Linux-x86
+CND_CONF=Release_Linux
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/Station.o \
+	${OBJECTDIR}/main.o \
+	${OBJECTDIR}/Explosion.o \
+	${OBJECTDIR}/SolarSystem.o \
+	${OBJECTDIR}/Ship.o \
+	${OBJECTDIR}/ShipCamera.o \
+	${OBJECTDIR}/Galaxy.o
+
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=../skyfire/dist/Release_Linux/GNU-Linux-x86/libskyfire.a
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/orcid
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/orcid: ../skyfire/dist/Release_Linux/GNU-Linux-x86/libskyfire.a
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/orcid: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.cc} -lpthread -lGLU -lSDL -ljpeg -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/orcid ${OBJECTFILES} ${LDLIBSOPTIONS} 
+
+${OBJECTDIR}/Station.o: Station.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O3 -w -I../skyfire -MMD -MP -MF $@.d -o ${OBJECTDIR}/Station.o Station.cpp
+
+${OBJECTDIR}/main.o: main.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O3 -w -I../skyfire -MMD -MP -MF $@.d -o ${OBJECTDIR}/main.o main.cpp
+
+${OBJECTDIR}/Explosion.o: Explosion.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O3 -w -I../skyfire -MMD -MP -MF $@.d -o ${OBJECTDIR}/Explosion.o Explosion.cpp
+
+${OBJECTDIR}/SolarSystem.o: SolarSystem.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O3 -w -I../skyfire -MMD -MP -MF $@.d -o ${OBJECTDIR}/SolarSystem.o SolarSystem.cpp
+
+${OBJECTDIR}/Ship.o: Ship.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O3 -w -I../skyfire -MMD -MP -MF $@.d -o ${OBJECTDIR}/Ship.o Ship.cpp
+
+${OBJECTDIR}/ShipCamera.o: ShipCamera.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O3 -w -I../skyfire -MMD -MP -MF $@.d -o ${OBJECTDIR}/ShipCamera.o ShipCamera.cpp
+
+${OBJECTDIR}/Galaxy.o: Galaxy.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O3 -w -I../skyfire -MMD -MP -MF $@.d -o ${OBJECTDIR}/Galaxy.o Galaxy.cpp
+
+# Subprojects
+.build-subprojects:
+	cd ../skyfire && ${MAKE}  -f skyfire-Makefile.mk CONF=Release_Linux
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/orcid
+
+# Subprojects
+.clean-subprojects:
+	cd ../skyfire && ${MAKE}  -f skyfire-Makefile.mk CONF=Release_Linux clean
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
