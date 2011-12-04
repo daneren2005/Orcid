b4c4772b5f6a4e853a15c5f269c6c7c3c131af0a
Scott Jackson <daneren2005@gmail.com>
2011-12-04 00:37:49 -0800

    Added more to DataFile tests

--- testDataFile.h
+++ testDataFile.h
@@ -7,11 +7,25 @@ void writeDataFile()
 {
 	DataFile units(String("data/units.dat"));
 	
-	DataFile::Object obj("Ship1");
-	obj.properties.insert("data/Danube/danube.obj", "model");
+	DataFile::Object* obj = new DataFile::Object("Ship1");
+	obj->properties.insert("data/Danube/danube.obj", "model");
+	
+	DataFile::Object* obj2 = new DataFile::Object("Station1");
+	obj2->properties.insert("data/Danube/danube.obj", "model");
+	
+	DataFile::Object* obj3 = new DataFile::Object("Ship2");
+	obj3->properties.insert("data/Danube/danube.obj", "model");
 	
 	units.insertObject(obj);
+	units.insertObject(obj2);
+	units.insertObject(obj3);
 	units.save();
+}
+
+void readDataFile()
+{
+	DataFile units(String("data/units.dat"));
+	units.load();
 }
 
 #endif	/* TESTDATAFILE_H */
