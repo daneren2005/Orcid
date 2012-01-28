ace2f000b2bd5f8ca7c48287c243efce855e83d3
Scott Jackson <daneren2005@gmail.com>
2012-01-27 19:42:14 -0800

    Added testThread to test both Thread and ThreadManager classes

---
+++ testThread.h
@@ -0,0 +1,50 @@
+#ifndef _TEST_THREAD_H
+#define	_TEST_THREAD_H
+
+#include "ThreadManager.h"
+#include "Thread.h"
+
+void nothing(Thread* arg)
+{
+
+}
+
+void SampleThread(Thread* arg)
+{
+	console << "Ticks per second: " << arg->getTicksPerSecond() << newline;
+}
+void sampleJob(GenericType arg)
+{
+	Sleep(rand() % 20 * 100);
+	console << "Job Done" << newline;
+	*((int*)arg) = *((int*)arg) + 1;
+}
+
+void testThread()
+{
+	Thread thread;
+	thread.start(SampleThread, 5);
+	// thread.setTicksPerSecond(10);
+	thread.setTimeBetweenTicks(2);
+	
+	Thread main;
+	main.setTicksPerSecond(1);
+	main.startMain(nothing, 0);
+}
+
+void testThreadManager()
+{
+	ThreadManager manager;
+	manager.setWorkers(2);
+	int* j = new int(0);
+
+	for(int i = 0; i < 5; i++)
+	{
+		manager.addJob(sampleJob, j);
+	}
+
+	manager.waitTillDone();
+	console << "Finished jobs: " << *j << newline;
+}
+
+#endif
