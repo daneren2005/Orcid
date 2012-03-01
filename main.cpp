b1288b8b023581a88230d1880690591a373a838d
Scott Jackson <daneren2005@gmail.com>
2012-02-29 21:30:07 -0800

    Removed start from window in main

--- main.cpp
+++ main.cpp
@@ -9,8 +9,6 @@
 #include "Galaxy.h"
 #include "ShipCamera.h"
 
-#include <iostream>
-
 #ifdef WIN32
 	int WINAPI WinMain(HINSTANCE program, HINSTANCE prevProgram, LPSTR args, int nCmdShow)
 #endif
@@ -18,19 +16,7 @@
 	int main(int argc,char** argv)
 #endif
 {
-	/*String str1 = ",,apple,blitz,,,mtez,,,,replace,";
-	String str2 = ",,";
-	String str3 = "-";
-	String str4 = str1.replace(",", "..");
-	std::cout << str4.cStr() << std::endl;*/
-
-	/*String str1 = "test";
-	String str3 = " - ";
-	String str2 = str1.insert(3, " - ");
-	std::cout << str2.cStr() << std::endl;*/
-
 	Window win(1280, 720);
-	win.start();
 
 	Galaxy galaxy;
 	galaxy.load();
@@ -39,12 +25,12 @@
 	Input* input = win.getInput();
 	camera->setEventHandlers(input);
 
-	win.setCamera(camera);
+	win.setRenderer(camera);
 	galaxy.setInput(input);
 	galaxy.start();
 
 	// Set program into infinite loop until window thread is stopped
 	win.wait();
 
-	return(0);
+	exit(0);
 }
